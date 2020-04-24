af907a323400a6f092d2bac138a358c257cd89c3
Daniel de Sales <ssales.daniel@gmail.com>
2020-04-24 16:43:26 -0300

    inseri input e buttons

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 
 export default function Login() {
+  const onSubmit = function (e) {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <div className="login">
-        <div className="login__container">
-          <div className="login__logo"></div>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="login">
+          <div className="login__container">
+            <div className="login__logo"></div>
+            <input
+              className="login__input"
+              type="input"
+              name="username"
+              placeholder="Usuário"
+            />
+            <input
+              className="login__input"
+              type="password"
+              name="password"
+              placeholder="Senha"
+            />
+            <button type="submit" className="login__button">
+              Entrar
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
