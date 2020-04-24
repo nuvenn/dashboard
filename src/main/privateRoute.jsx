c0d6c7134e0857f61c751522c613ac089cb50416
Daniel de Sales <ssales.daniel@gmail.com>
2020-04-24 15:26:09 -0300

    adicionei tela login aiinda sem layout

--- src/main/privateRoute.jsx
+++ src/main/privateRoute.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
+import _ from "lodash";
 
 import Layout from "../components/layout/layout";
+import Login from "../pages/login/login";
 
 export default function PrivateRoute(props) {
+  const [permissions, setPermissions] = useState({});
+
+  const getPermissions = async () => {
+    let response = await axios.get("api/permissions");
+    let { data } = response;
+    setPermissions({ permissions: data });
+  };
+
+  useEffect(() => {
+    getPermissions();
+  }, []);
+
   return (
     <>
-      <Layout title={props.title}>
-        <Route {...props} />
-      </Layout>
+      {!_.isEmpty(permissions) ? (
+        <Layout title={props.title}>
+          <Route {...props} />
+        </Layout>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
