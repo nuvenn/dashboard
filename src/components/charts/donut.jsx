438976259788b0fe2971482480385c9e0279ba31
Daniel de Sales <ssales.daniel@gmail.com>
2020-04-24 14:50:33 -0300

    mudei cores do pie

--- src/components/charts/donut.jsx
+++ src/components/charts/donut.jsx
@@ -6,7 +6,7 @@ export default function DonutChart(props) {
 
   const chart = {
     options: {
-      colors: ["#701F38", "#D69F92", "#70301F", "#4F7D42", "#6AA31D"],
+      colors: ["#701F38", "#D69F92", "#70301F", "#ffd6e2", "#D23666"],
     },
     series: [44, 55, 41, 17, 15],
     labels: ["A", "B", "C", "D", "E"],
