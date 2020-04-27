bcc1a80b2aefc31046d741284b643cf4906fa65a
Daniel de Sales <ssales.daniel@gmail.com>
2020-04-27 16:25:39 -0300

    alterei para yarn

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Switch, Redirect } from "react-router-dom";
-import Home from "../pages/home/home";
+// import Home from "../pages/home/home";
 import QuantidadeVinculos from "../pages/quantidadeVinculos/quantidadeVinculos";
 import TotaisFolha from "../pages/totaisFolha/totaisFolha";
 import PrivateRoute from "./privateRoute";
@@ -8,12 +8,12 @@ import PrivateRoute from "./privateRoute";
 export default () => (
   <Router>
     <Switch>
-      <PrivateRoute
+      {/* <PrivateRoute
         path="/home/"
         title="Bem vindo ao Cockpit"
         exact
         component={Home}
-      />
+      /> */}
       <PrivateRoute
         path="/quantidadevinculos/"
         title="Quantidade de Vínculos"
@@ -26,7 +26,7 @@ export default () => (
         exact
         component={TotaisFolha}
       />
-      <Redirect from="*" to="/home/" />
+      <Redirect from="*" to="/quantidadevinculos/" />
     </Switch>
   </Router>
 );
